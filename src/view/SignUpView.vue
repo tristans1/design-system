5229ca39b9bb47b7ed52c9419403352a7faf2700
Jessy <ernatusjessy@gmail.com>
2022-02-04 15:41:13 +0100

    add theses files in project

---
+++ src/view/SignUpView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>
+        SignUpView
+    <h1>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
