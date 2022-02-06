42cfd66c206c426ca9caddfab8d68416390480f9
Tristan Seclet <tristanseclet@gmail.com>
2022-02-06 17:18:32 +0100

    added new views + removed HelloWorld.vue

---
+++ src/view/SignUpView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Hey</h1>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'SignUpView',
+    };
+</script>
+<style scoped lang="scss"></style>
