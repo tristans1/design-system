c26a3de946aeede0859e0dc986f75ee378736d73
Tristan Seclet <tristanseclet@gmail.com>
2022-02-06 23:13:49 +0100

    added components : FlexContainer, FormContainer

---
+++ src/components/FormContainerComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <form class="form-container">
+        <slot></slot>
+    </form>
+</template>
+<script>
+    export default {
+        name: 'FormContainerComponent',
+    };
+</script>
+<style scoped lang="scss">
+    @import "../assets/scss/variable";
+
+    .form-container {
+        border-radius: $form-border-radius;
+        box-shadow: $form-box-shadow;
+        background-color: $form-bg-color;
+        max-width: $form-width;
+        width: 100%;
+        margin: $spacing-auto;
+        padding: $spacing-l;
+    }
+</style>
