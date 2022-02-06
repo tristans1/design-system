fe4ef36bdb4ab21d25662c3a40ee97382b76dc28
Tristan Seclet <tristanseclet@gmail.com>
2022-02-06 23:19:51 +0100

    added components : EyeOpenIcon.vue, EyeCloseIcon.vue

---
+++ src/components/icons/EyeOpenIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <Icon class="icon" icon="akar-icons:eye"/>
+</template>
+<script>
+    import {Icon} from '@iconify/vue2';
+    export default {
+        name: 'EyeOpenIcon',
+        components: {
+            Icon,
+        },
+
+    };
+</script>
+<style scoped lang="scss">
+     @import "../../assets/scss/variable";
+
+     .icon {
+         color: $icon-color;
+         font-size: $icon-size;
+     }
+</style>
